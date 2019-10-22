800f4843279fb36229dcfa5a9e28259778653a33
Quantumapple <jongho.lee@cern.ch>
2019-10-22 13:34:06 -0500

    fitting with linear function on channel 1

--- PythonAnalysis/read.py
+++ PythonAnalysis/read.py
@@ -1,7 +1,7 @@
 import h5py
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import optimize
 
 verbose = False
 
@@ -17,28 +17,35 @@ dataset = f['C0']
 rng = f.attrs['Waveform Attributes'].tolist()
 
 if verbose :
-    print "What's inside in the 'waveform attributes': "
-    print "channel mask"+",", "number of points"+",", "number of frames"+",", u"\N{GREEK CAPITAL LETTER DELTA}"+"t"+",", "t0"+",", "y multiply values"+",", "y offsets"+",", "y zero"
-    print rng
+    print("What's inside in the 'waveform attributes': ")
+    print("channel mask"+",", "number of points"+",", "number of frames"+",", u"\N{GREEK CAPITAL LETTER DELTA}"+"t"+",", "t0"+",", "y multiply values"+",", "y offsets"+",", "y zero")
+    print(rng)
 
 npoints = rng[1]
 dt = rng[3]
 ymult = 0.004
 
-xarray = np.arange(rng[1])*rng[3]
+xmin = (int)(npoints/2-10)
+xmax = (int)(npoints/2+10)
 
-if verbose :
-    print xarray
-
-ch1 = np.arange(dataset[0, rng[1]:rng[1]*2])*ymult
-#ch2 = np.arange(dataset[1, rng[1]:rng[1]*2])*ymult
-
-
-#print(np.arange(rng[1]*2-rng[1])*rng[3])
-
-#plt.plot(np.arange(rng[1]*2)*rng[3], dataset[0, rng[1]*1:rng[1]*3]*0.004)
+#plt.figure(figsize=(12,8))
+#plt.scatter(np.arange(xmin, xmax), dataset[0, xmin:xmax]*ymult)
+#plt.scatter(np.arange(npoints), dataset[0, npoints:npoints*2]*ymult)
 #plt.plot(np.arange(rng[1])*rng[3], dataset[0, rng[1]*1:rng[1]*2])
-#plt.show()
+#plt.plot(np.arange(npoints)*dt, dataset[0, npoints:npoints*2]*ymult)
+
+def test_func(x, a, b):
+    return a * x + b
+
+params, params_covariance = optimize.curve_fit(test_func, np.arange(xmin, xmax), dataset[0, xmin:xmax]*ymult, p0=[1,1])
+print(params)
+
+plt.figure(figsize=(12,8))
+plt.scatter(np.arange(xmin, xmax), dataset[0, xmin:xmax]*ymult, label='Data')
+plt.plot(np.arange(xmin, xmax), test_func(np.arange(xmin, xmax), params[0], params[1]),
+        label='Fitted function')
+plt.legend(loc='best')
+plt.show()
 
 
 
