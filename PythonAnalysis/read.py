38919ded76c53e51638959787ca8aeec951421e7
Quantumapple <jongho.lee@cern.ch>
2019-10-23 22:05:32 -0500

    find t1 with 3rd order polynomial

--- PythonAnalysis/read.py
+++ PythonAnalysis/read.py
@@ -37,24 +37,33 @@ xmax = (int)(850)
 params, params_covariance = optimize.curve_fit(poly3d, np.arange(xmin, xmax), dataset[1, xmin:xmax]*ymult, p0=[1,1,1,1])
 print(params)
 
-#ymax = optimize.fmin(lambda x: -poly3d(np.arange(xmin, xmax), params[0], params[1], params[2], params[3]), 0)
-#print(ymax)
+def f(x):
+    return -(params[0]*pow(x,3)+params[1]*pow(x,2)+params[2]*pow(x,1)+params[3])
 
-plt.figure(figsize=(12,8))
-#plt.scatter(np.arange(xmin, xmax), dataset[1, xmin:xmax]*ymult, label='Data')
-plt.scatter(np.arange(npoints), dataset[1, npoints*0:npoints*1]*ymult, label='Data')
-#plt.plot(np.arange(xmin, xmax), poly1d(np.arange(xmin, xmax), params[0], params[1]), label='Fitted function')
-plt.plot(np.arange(xmin, xmax), poly3d(np.arange(xmin, xmax), params[0], params[1], params[2], params[3]), label='Fitted function', c='r')
-plt.legend(loc='best')
-plt.grid(True)
-plt.title("Scope")
-plt.xlabel("time(s)")
-plt.ylabel("amplitude(mV)")
+maximumX = optimize.fminbound(f, 825, 850)
+maximumY = -f(maximumX)
+print(maximumX, maximumY, maximumY*0.8)
+
+def f2(x):
+    return params[0]*pow(x,3)+params[1]*pow(x,2)+params[2]*pow(x,1)+params[3]-(maximumY*0.8)
+
+root = optimize.root(f2, x0=830)
+print(root.x*dt)
+t1 = root.x*dt
+
+print("Time difference:", t1-t0)
+
+plt.hist(t1-t0, 'auto')
 plt.show()
-#plt.savefig('fig_scope_ch2_2nd.pdf')
-#print("t0: ", t0)
-#ymax = test_func(xmax, params[0], params[1])
-#yt1= ymax*0.8
-#t1 = (yt1 - params[1])/params[0]
-#print(t1*dt)
-#print(t1*dt-t0)
+
+#plt.figure(figsize=(12,8))
+#plt.scatter(np.arange(xmin, xmax), dataset[1, xmin:xmax]*ymult, label='Data')
+#plt.scatter(np.arange(npoints), dataset[1, npoints*0:npoints*1]*ymult, label='Data')
+#plt.plot(np.arange(xmin, xmax), poly1d(np.arange(xmin, xmax), params[0], params[1]), label='Fitted function')
+#plt.plot(np.arange(xmin, xmax), poly3d(np.arange(xmin, xmax), params[0], params[1], params[2], params[3]), label='Fitted function', c='r')
+#plt.legend(loc='best')
+#plt.grid(True)
+#plt.title("Scope")
+#plt.xlabel("time(s)")
+#plt.ylabel("amplitude(mV)")
+#plt.show()
